588d49f76141c985bcd19ea9d98d08a35334c60c
Logan <logalamo@gmail.com>
2021-03-18 20:41:13 -0500

    Added the extra customer views and routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,13 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def alphabetized
+    @customers = Customer.all.order("full_name ASC")
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: [nil, ""])
+  end
+
 end
